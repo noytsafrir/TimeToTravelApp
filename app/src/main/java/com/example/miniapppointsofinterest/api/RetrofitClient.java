dcdbfc320ec1681b43ee93fd20f7bb922f42aaf1
NoyTsafrir <noy221999@gmail.com>
2023-06-04 12:42:03 +0300

    change view- match to screen

--- app/src/main/java/com/example/miniapppointsofinterest/api/RetrofitClient.java
+++ app/src/main/java/com/example/miniapppointsofinterest/api/RetrofitClient.java
@@ -10,8 +10,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit = null;
-//    private static final String BASE_URL = "http://172.20.22.46:8081/";
-    private static final String BASE_URL = "http://192.168.1.199:8081/";
+    private static final String BASE_URL = "http://172.20.23.77:8081/";
+//    private static final String BASE_URL = "http://192.168.1.199:8081/";
 
     private RetrofitClient() {}// Private constructor to prevent instantiation from outside
 
